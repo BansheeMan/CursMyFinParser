ae217647cfc8e3d8ba6a92d8c0e248a479ecd325
ManBanshee <klindyuk98@gmail.com>
2022-08-18 02:09:26 +0300

    Get dollar value

--- app/src/main/java/com/example/cursmyfinparser/MainActivity.kt
+++ app/src/main/java/com/example/cursmyfinparser/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.cursmyfinparser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.cursmyfinparser.databinding.ActivityMainBinding
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -15,6 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
-        binding.textView.text =  "Дарова"
+
+        Thread { getWeb() }.start()
+        //binding.textView.text =  doc.title().toString()
+        Log.d("@@@", "####################################")
+
+    }
+
+    private fun getWeb() {
+        doc = Jsoup.connect("https://myfin.by/currency/minsk").get()
+        val table = doc.getElementsByTag("tbody")
+        val ourTable = table[0]
+        val elementsFromTable = ourTable.children()[0].text()
+        val elem_dollar = ourTable.children()[0].children()[1].text()
+
+        Log.d("@@@", "Title: ${doc.title()}")
+        Log.d("@@@", "####################################")
+        Log.d("@@@", "${elementsFromTable}")
+        Log.d("@@@", "####################################")
+        Log.d("@@@", "${elem_dollar}")
+
+
     }
 }
